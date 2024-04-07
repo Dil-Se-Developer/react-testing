6a2d590dc3403802c8a366c32137b0f8dbbad8ba
Shail <shailshah.officework@gmail.com>
2024-04-07 22:56:27 +0530

    leart_react_testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Application } from "./components/application/application";
+import { Skills } from "./components/skills/skills";
 
 function App() {
+  const skills = ["HTML", "CSS", "JavaScript"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Application /> */}
+      <Skills skills={skills} />
     </div>
   );
 }
---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,18 @@
+import { Greet } from "./greet";
+import { render, screen } from "@testing-library/react";
+
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+});
+
+describe("Nested", () => {
+  test("renders a name", () => {
+    render(<Greet name="Shail" />);
+    const textElement = screen.getByText("Hello Shail");
+    expect(textElement).toBeInTheDocument();
+  });
+});
