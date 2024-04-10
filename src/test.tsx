71433628d3b860b1e60f62c33469db59ed537ea2
Shail_Shah_Personal <shailshah.officework@gmail.com>
2024-04-10 09:42:59 +0530

    test_git

---
+++ src/test.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Application } from "./components/application/application";
+import { Skills } from "./components/skills/skills";
+
+function App() {
+  const skills = ["HTML", "CSS", "JavaScript"];
+
+  return (
+    <div className="App">
+      {/* <Application /> */}
+      <Skills skills={skills} />
+    </div>
+  );
+}
+
+export default App;
