cb1f742089bfa90180fd2da6d4410df487185dc7
Shail_Shah_Personal <shailshah.officework@gmail.com>
2024-04-11 17:47:14 +0530

    provider_test_setup

--- Theory/KeyboardInteractions.md
+++ Theory/KeyboardInteractions.md
@@ -1,3 +1,35 @@
 #Keyboard Interactions
 
 **user-events are asynchronous**
+
+**For geting input type 'number' from DOM use 'getByRole('spinbutton')'**
+
+## Utility APIs
+
+- type()
+
+* clear()
+
+* selectOptions()
+
+* deselectOptions()
+
+* upload()
+
+## Convenience APIs
+
+- tab()
+
+## Clipboard APIs
+
+- copy()
+
+- cut()
+
+- paste()
+
+## Keyboard APIs
+
+- keyboard('foo')
+
+* keyboard('{Shift>}A{/Shift}')
--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import "./App.css";
 import { Application } from "./components/application/application";
 import { Counter } from "./components/counter/counter";
 import { Skills } from "./components/skills/skills";
+import { MuiMode } from "./components/mui/MuiMode";
+import { AppProviders } from "./providers/AppProviders";
 
 function App() {
   const skills = ["HTML", "CSS", "JavaScript"];
 
   return (
-    <div className="App">
-      {/* <Application /> */}
-      {/* <Skills skills={skills} /> */}
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        {/* <Application /> */}
+        {/* <Skills skills={skills} /> */}
+        {/* <Counter /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
